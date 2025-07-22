7f96cd3aeadff8c5f15295e6a23cc4a085041144
Àlex Meléndez <alex27mel@gmail.com>
2025-07-22 18:57:01 +0200

    patch
    patch

--- src/data/director.js
+++ src/data/director.js
@@ -2,17 +2,17 @@ const carDealerships = [
   {
     name: "Denis Villeneuve",
     age: 57,
-    img: "",
+    img: "https://res.cloudinary.com/djsoi0rry/image/upload/v1749656025/API_REST_FILES_PROJECT/bbn36rl3lm9ibhaeugbi.jpg",
   },
   {
     name: "Justin Lin",
     age: 53,
-    img: "",
+    img: "https://res.cloudinary.com/djsoi0rry/image/upload/v1749656420/API_REST_FILES_PROJECT/wi4ixx9fyichzagcf4nr.jpg",
   },
   {
     name: "George Lucas",
     age: 81,
-    img: "",
+    img: "https://res.cloudinary.com/djsoi0rry/image/upload/v1749656669/API_REST_FILES_PROJECT/tmcp4nvcxiwuouohjhdk.jpg",
   }
 ];
 
